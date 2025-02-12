7f8ebff8a53776bdd12591cbce362b6a0a42b166
Jesus David Lozano <jesusdavidlozanomarin@gmail.com>
2025-02-12 15:46:20 -0400

    Correcion de estilos en consultar asignaciones.

--- src/pages/CheckAsign.jsx
+++ src/pages/CheckAsign.jsx
@@ -13,22 +13,23 @@ const CheckInfo = () => {
     }
 
     return(
-        <div className="CheckInfo">
+        <div className="CheckAsign">
             <h1 className="purple">Consultar informacion</h1>
             <Input.Search size="large" placeholder="Ingrese cedula a consultar" id="searchParam" onSearch={e => getInfo(e)}/>
 
             { info != null && (info.type == 1 || info.type == 2) ? (
-                <>
-                    <h2 className="purple">Cedula: {info.identification} </h2>
-                    <h2 className="purple">Nombre: {info.name} {info.lastname} </h2>
-                    <h2 className="purple">
+                <div className="infoContainer">
+                    <h1>Informacion del {info.type == 1 && <>docente</>}{info.type == 2 && <>estudiante</>}</h1>
+                    <h2>Cedula: {info.identification} </h2>
+                    <h2>Nombre: {info.name} {info.lastname} </h2>
+                    <h2>
                         Es {info.type == 1 && <>profesor</>}{info.type == 2 && <>estudiante</>} en la seccion {info.section} de {info.asignature}
                     </h2>
-                </> 
+                </div> 
             ):(
-                <>
-                    <h1 className="purple">No hay informacion para mostrar</h1>
-                </>
+                <div className="infoContainer">
+                    <h1>No hay informacion para mostrar</h1>
+                </div>
             ) }
         </div>
     )
